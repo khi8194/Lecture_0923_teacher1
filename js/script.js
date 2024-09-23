a3e5e4a3aaaa863e8e8e1b200624a40ba036a60e
tubi55 <tubi55@nate.com>
2024-09-23 14:10:14 +0900

    시간관련 테마 데이터 배열로 분리

--- js/script.js
+++ js/script.js
@@ -36,22 +36,25 @@ function setTime(num, index) {
 function changeTheme() {
   const hr = new Date().getHours();
 
+  const themeData = [
+    { className: "morning", period: [5, 11] },
+    { className: "afternoon", period: [11, 16] },
+    { className: "evening", period: [16, 20] },
+    { className: "night", period: [20, 5] },
+  ];
+
+  main.className = "";
+
   if (hr >= 5 && hr < 11) {
-    main.className = "";
     main.classList.add("morning");
   }
   if (hr >= 11 && hr < 16) {
-    main.className = "";
     main.classList.add("afternoon");
   }
   if (hr >= 16 && hr < 20) {
-    main.className = "";
     main.classList.add("evening");
   }
   if (hr >= 20 || hr < 5) {
-    main.className = "";
     main.classList.add("night");
   }
 }
-//미션 - 만약 오후 1시가 넘어가면 (13시 이상일때 다시 01로 표기법 변경)
-// 1시 35분까지
