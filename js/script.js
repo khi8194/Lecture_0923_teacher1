f9a145ad8b2a1ee26f0f3a77c31abce19ed446fe
tubi55 <tubi55@nate.com>
2024-09-23 10:35:41 +0900

    배경 및 왼쪽 텍스트 영역 스타일링

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
-/*
-  setInterval : 특정 함수를 일정시간마다 반복 호출
-
-  setInterval(함수,인터벌시간) : 인터벌간격마다 함수 반복 호출
-*/
-
-const title = document.querySelector("h1");
-const [em, spanHr, spanMin, spanSec] = title.children;
+const screen = document.querySelector(".screen");
+const [spanHr, spanMin, spanSec] = screen.children;
 
 //1초마다 시간출력 반복
 setInterval(() => {
@@ -13,9 +7,7 @@ setInterval(() => {
   const hr = now.getHours();
   const min = now.getMinutes();
   const sec = now.getSeconds();
-  let apm = hr < 13 ? "am" : "pm";
 
-  em.innerText = apm;
   spanHr.innerText = hr;
   spanMin.innerText = min;
   spanSec.innerText = sec;
